02deafa9a7ecd6c799b6f5dd3a56f629a8a37c07
MrAzarel <arseniy.mazurenko.03@gmail.com>
2022-11-18 00:20:20 +0300

    Добавьте файлы проекта.

---
+++ 9_3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Reflection.Metadata;
+
+enum ClothingSize
+{
+    XXS = 32,
+    XS = 34,
+    S = 36,
+    M = 38,
+    L = 40
+}
+
+public static class Age
+{
+    public static string SizeToAge(Constant size)
+    {
+        if (size.Equals(32)||size.Equals(34))
+            return "Детский размер";
+        else
+            return "Взрослый размер";
+    }
+}
+
+public interface IMansClothing
+{
+    public void DressAMan()
+    {
+
+    }
+}
+
+public interface IWomensClothing
+{
+    public void DressAWoman()
+    {
+
+    }
+}
+
+abstract class Clothes
+{
+    int size;
+    double price;
+    string color;
+}
+
+class TShirt : Clothes 
+{
+
+}
+
+class Pants : Clothes
+{
+
+}
+
+class Tie : Clothes
+{
+
+}
+
+class Atelier
+{
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
